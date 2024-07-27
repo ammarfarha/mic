6027b79f10ff1baa9512fcd8748caa604d6e0f81
Ali Rajha <arajha@nurvu.com>
2024-07-28 00:45:32 +0300

    creating routes to create and store students

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/create' , [StudentController::class , 'create'])->name('createstudent');
+Route::post('/store' , [StudentController::class , 'store'])->name("storestudent");
